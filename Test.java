18b3ddf2db637a7470344113e37f2e2d7049985e
tatev95 <tatev.torosyan@yahoo.com>
2021-03-28 14:53:55 +0400

    First Homework

---
+++ Test.java
@@ -0,0 +1,28 @@
+public class Test {
+    public static void main(String[] args) {
+        byte n1 = -12;
+        System.out.println(n1);
+        n1 = 127;
+        System.out.println(n1);
+        short n2 = 564;
+        System.out.println(n2);
+        int n3 = 23456788;
+        System.out.println(n3);
+        long n4 = 5858587455L;
+        System.out.println(n4);
+        float n5 = 2.23F;
+        System.out.println(n5);
+        double n6 = 256.78787878;
+        System.out.println(n6);
+        char l1 = 'T';
+        System.out.println(l1);
+        boolean b = false;
+        System.out.println(b);
+
+        String s1 = "Hello World";
+        System.out.println(s1);
+        String name = "Tatevik";
+        String surname = "Torosyan";
+        System.out.println(name + ' ' + surname);
+    }
+}
